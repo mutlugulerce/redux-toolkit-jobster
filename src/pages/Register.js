f317ab26199b9c8af2cad601fa555d2bd95f8d2e
Mutlu Gülerce <118522613+mutlugulerce@users.noreply.github.com>
2023-02-13 10:13:42 +0300

    ilk yükleme

---
+++ src/pages/Register.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
+const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3> {values.isMember ? "Login" : "Register"} </h3>
+
+        {values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+     <button
+            type="submit"
+            className="btn btn-block"
+            
+          >
+          Submit
+          </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button
+            type="button"
+           className="member-btn"
+            onClick={toggleMember}
+          >
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Register;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import Error from "./Error";
+import Register from "./Register";
+
+
+
+export {Error,Dashboard,Landing,Register}
