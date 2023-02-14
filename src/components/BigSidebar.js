d1485849ecd564ee258e400d5382a3ce2a6a9b75
Mutlu Gülerce <118522613+mutlugulerce@users.noreply.github.com>
2023-02-14 16:15:28 +0300

    dashboard componetleri ve profili oluşturma

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,11 +1,25 @@
 import Wrapper from '../assets/wrappers/BigSidebar'
+import { useSelector } from 'react-redux'
+import Logo from './Logo'
+import NavLinks from './NavLinks';
 
 const BigSidebar = () => {
+  const {isSidebarOpen} = useSelector((store) => store.user)
   return (
     <Wrapper>
-    <h1>
-      bigsidebar
-    </h1>
+    <div
+    className={isSidebarOpen ? 'sidebar-container' : 'sidebar-container show-sidebar'}
+    >
+   <div className="content">
+    <header>
+      <Logo/>
+    </header>
+    <NavLinks />
+   </div>
+    </div>
+
+
+   
     </Wrapper>
   )
    
--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,11 +1,32 @@
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import {FaTimes} from 'react-icons/fa'
+import NavLinks from './NavLinks';
+import Logo from './Logo'
+import { useSelector,useDispatch } from 'react-redux'
+import {toggleSidebar} from '../features/user/userSlice'
+
 
 const SmallSidebar = () => {
+  const {isSidebarOpen} = useSelector((store) => store.user)
+  const dispatch= useDispatch()
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  }
   return (
     <Wrapper>
-  <h1>
-      smallsidebar
-    </h1>
+     <div className={isSidebarOpen? "sidebar-container show-sidebar" : 'sidebar-container'}>
+      <div className="content">
+  <button className="close-btn" onClick={toggle}><FaTimes/></button>
+      <header>
+        <Logo/>
+      </header>
+      <NavLinks toggleSidebar={toggle}/>
+      </div>
+    
+      </div>
+    
+
     </Wrapper>
   
   )
